a601163e76619d01333e70e7a45a146078de94dc
Andrea Zonca <code@andreazonca.com>
2017-11-08 02:13:24 -0800

    set overlay network

--- launch_letsencrypt_container.sh
+++ launch_letsencrypt_container.sh
@@ -7,5 +7,6 @@ docker run \
   -e EMAIL=your_email@domain.edu \
   -e URL=your.domain.org \
   -v nginx_volume:/config \
+  --network jupyterhub \
   --mount type=bind,src=/etc/jupyterhub/letsencrypt_container_nginx.conf,dst=/config/nginx/site-confs/default \
   linuxserver/letsencrypt
