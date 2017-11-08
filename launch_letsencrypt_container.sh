ec824d3f26f590301ad878427008097e13ad15db
Andrea Zonca <code@andreazonca.com>
2017-11-08 02:12:39 -0800

    letsencrypt container

---
+++ launch_letsencrypt_container.sh
@@ -0,0 +1,11 @@
+docker run \
+  --cap-add=NET_ADMIN \
+  --name nginx \
+  -p 443:443 \
+  -p 80:80 \
+  --detach \
+  -e EMAIL=your_email@domain.edu \
+  -e URL=your.domain.org \
+  -v nginx_volume:/config \
+  --mount type=bind,src=/etc/jupyterhub/letsencrypt_container_nginx.conf,dst=/config/nginx/site-confs/default \
+  linuxserver/letsencrypt
